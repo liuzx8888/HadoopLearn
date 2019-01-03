7d8ccf662fbbc59cba8affdb746eea70fdbcc1b6
Administrator <Administrator@Liuzx>
2019-01-03 17:42:53 +0800

    111

---
+++ src/main/java/com/hadoop/learn/hadoop_action/App.java
@@ -0,0 +1,13 @@
+package com.hadoop.learn.hadoop_action;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
